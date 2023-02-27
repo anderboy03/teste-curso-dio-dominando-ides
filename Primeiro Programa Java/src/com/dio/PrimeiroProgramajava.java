9f3fc9b1394f2bfd7c2a0a78b87a7c9a67fb4ede
Anderson Clayton Ribas <andersex03@gmail.com>
2023-02-27 17:30:07 -0300

    firts commit

---
+++ Primeiro Programa Java/src/com/dio/PrimeiroProgramajava.java
@@ -0,0 +1,10 @@
+package com.dio;
+
+public class PrimeiroProgramajava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world " + (2+3));
+
+	}
+
+}
---
+++ Primeiro Programa Java/src/com/dio/model/gato.java
@@ -0,0 +1,30 @@
+package com.dio.model;
+
+public class gato {
+	private string nome;
+	private String cor;
+	private integer idade;
+
+	public gato() {
+	}
+
+	public gato(string nome, String cor, integer idade) {
+		super();
+		this.nome = nome;
+		this.cor = cor;
+		this.idade = idade;
+	}
+
+	public gato(string nome, String cor, integer idade) {
+		super();
+		this.nome = nome;
+		this.cor = cor;
+		this.idade = idade;
+	}
+
+}
+
+/*
+ * public gato(string nome, string cor, integer idade) { this.nome= nome;
+ * this.cor= cor; this.idade = idade; }
+ */
